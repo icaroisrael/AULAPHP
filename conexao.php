cd8d4322ff7349f6265a2ea0eda7b4ac53f10dd9
Israel Furtado <icaroisrael@hotmail.com>
2022-09-13 07:58:16 -0300

    v1

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 
 try {
     $pdo = new PDO("mysql:host=localhost;dbname=agendamento", "root", "");
-    echo "Conectado com sucesso";
+    
 } catch (PDOException $e) {
     echo "Erro com o banco de dados".$e->getMessage();   
 }catch(Exception $e){
--- listar.php
+++ listar.php
@@ -1,10 +1,29 @@
 <?php
 require("conexao.php");
-$resultado = $pdo->query("SELECT * FROM USUARIO");
+$resultado = $pdo->query("SELECT email, nome, sobrenome FROM USUARIO");
+?>
+<table BORDER="1">
+    <tr>
+        <td>EMAIL</td>
+        <td>NOME</td>
+        <td>SOBRENOME</td>
+        <td>EDITAR</td>
+        <td>EXCLUIR</td>
+    </tr>
 
-
-while($linha = $resultado->fetch(PDO::FETCH_ASSOC)){
-    echo "EMAIL : {$linha['email']}";
+<?php 
+while($linha = $resultado->fetch(PDO::FETCH_ASSOC)){?>
+    <tr>
+        <td><?php echo "{$linha['email']}";?> </td>
+        <td><?php echo "{$linha['nome']}";?> </td>
+        <td><?php echo "{$linha['sobrenome']}";?> </td>
+        <td></td>
+        <td></td>
+    </tr>
+  <?php  
 }
+?>
+</table>
+<?php
 
 ?>
